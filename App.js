9fb78b047494531c10a23830b7d7ec605d2e453a
poppo4444 <aritomo44@icloud.com>
2021-04-09 15:02:50 +0900

    :sparkles: ヘッダーのスタイリング

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.appBar}>
+        <View style={styles.appBarInner}>
+          <Text style={styles.appBarCenter}>注文画面</Text>
+          <StatusBar style="light" />
+        </View>
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.header}>もも肉</Text>
+        <StatusBar style="light" />
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.header}>もも肉</Text>
+        <StatusBar style="light" />
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.header}>せせり</Text>
+        <StatusBar style="light" />
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.header}>ねぎま</Text>
+        <StatusBar style="light" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+  appBar: {
+    width : '100%',
+    height: 100,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+  appBarInner:{
+    alignItems: 'center',
+  },
+  appBarCenter: {
+    alignItems: 'center',
+    fontSize: 32,
+    marginBottom: 8,
+    lineHeight: 32,
+    color: '#ffffff'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#222',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  header: {
+    flex: 1,
+    fontSize: 22,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  }
 });
